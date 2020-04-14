a46351490466c3fe6d1ac31a10991a4468fa484c
Jaber Jaber <jaber_jaber@hotmail.com>
2020-04-13 21:39:12 -0700

    added req file

--- The_Social_Network/my_social_network/users/tests.py
+++ The_Social_Network/my_social_network/users/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client, TransactionTestCase
+
 
 # Create your tests here.
+class UserHistoryTest(TransactionTestCase):
+    self.user = User.objects.create(username='testuser', password='testpass', email='test@test.com')
+    self.client = Client()
+
+    def test_history(self):
+        self.client.login(username=self.user.username, password='testpass')
+        # get_history function having login_required decorator
+        response = self.client.post(reverse('get_history'), {'user_id': self.user.id})
+        self.assertEqual(response.status_code, 200)
